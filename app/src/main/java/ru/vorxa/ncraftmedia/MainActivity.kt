d2e8102da0c3f9af8fec9c19142c926a541cc0dc
vorxa <vorxa@yandex.ru>
2019-11-18 11:34:40 +0700

    20191118-1134

--- app/src/main/java/ru/vorxa/ncraftmedia/MainActivity.kt
+++ app/src/main/java/ru/vorxa/ncraftmedia/MainActivity.kt
@@ -1,9 +1,15 @@
 package ru.vorxa.ncraftmedia
 
+import android.content.Intent
 import android.graphics.Color
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
+import ru.vorxa.ncraftmedia.dto.Event
 import ru.vorxa.ncraftmedia.dto.Post
 import ru.vorxa.ncraftmedia.utils.*
 
@@ -14,11 +20,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val currentTime: Long = System.currentTimeMillis()/1000
-        val post1 = Post(
+        val post1 = Event(
             1,
             "Сан Саныч",
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+            "The old post office in Ny-Ålesund is not in official use anymore, but it is open and visitors are free to walk in.",
             currentTime - 700,
+            "Ny-Ålesund Post Office",
+            78.9237826 to 11.9079864,
             false,
             false,
             true,
@@ -33,26 +41,55 @@ class MainActivity : AppCompatActivity() {
         authorTextView.text = post.author
         contentTextView.text = post.content
 
-        if (post.likes > 0) {
-            likesTextView.text = post.likes.toString()
+        setButtons(post.likedByMe, post.likes, likesTextView, likeButton, "favorite")
+        setButtons(post.sharedByMe, post.shares, sharesTextView, shareButton, "share")
+        setButtons(post.commentedByMe, post.comments, commentsTextView, commentButton, "comment")
+
+        likeButton.setOnClickListener{
+            post.likedByMe=!post.likedByMe
+            if (post.likedByMe) {
+                post.likes += 1
+            } else {
+                post.likes -= 1
+            }
+            setButtons(post.likedByMe, post.likes, likesTextView, likeButton, "favorite")
         }
-        if (post.comments > 0) {
-            commentsTextView.text = post.comments.toString()
+        commentButton.setOnClickListener{
+            post.commentedByMe=!post.commentedByMe
+            if (post.commentedByMe) {
+                post.comments += 1
+            } else {
+                post.comments -= 1
+            }
+            setButtons(post.commentedByMe, post.comments, commentsTextView, commentButton, "comment")
         }
-        if (post.shares > 0) {
-            sharesTextView.text = post.shares.toString()
+        shareButton.setOnClickListener{
+            post.sharedByMe=!post.sharedByMe
+            if (post.sharedByMe) {
+                post.shares += 1
+            } else {
+                post.shares -= 1
+            }
+            setButtons(post.sharedByMe, post.shares, sharesTextView, shareButton, "share")
         }
-        if (post.likedByMe) {
-            likeButton.setImageResource(R.drawable.ic_favorite_red_24dp)
-            likesTextView.setTextColor(Color.RED)
+        geoButton.setOnClickListener {
+            val location = Uri.parse("geo:${post.location.first},${post.location.second}")
+            val intent = Intent(Intent.ACTION_VIEW).apply { data = location }
+            startActivity(intent)
+
         }
-        if (post.commentedByMe) {
-            commentButton.setImageResource(R.drawable.ic_comment_red_24dp)
-            commentsTextView.setTextColor(Color.RED)
+
+
+    }
+
+    private fun setButtons(byMe: Boolean, count: Int, textView: TextView, buttonView: ImageButton, type: String) {
+        textView.text = if (count > 0) count.toString() else ""
+        val image = when(type) {
+            "favorite" -> if (byMe) R.drawable.ic_favorite_red_24dp else R.drawable.ic_favorite_gray_24dp
+            "comment" -> if (byMe) R.drawable.ic_comment_red_24dp else R.drawable.ic_comment_gray_24dp
+            else -> if (byMe) R.drawable.ic_share_red_24dp else R.drawable.ic_share_gray_24dp
         }
-        if (post.sharedByMe) {
-            shareButton.setImageResource(R.drawable.ic_share_red_24dp)
-            sharesTextView.setTextColor(Color.RED)
-        }
+        buttonView.setImageResource(image)
+        textView.setTextColor(if (byMe) Color.RED else Color.GRAY)
     }
 }
--- app/src/main/java/ru/vorxa/ncraftmedia/dto/Event.kt
+++ app/src/main/java/ru/vorxa/ncraftmedia/dto/Event.kt
@@ -1,10 +1,12 @@
 package ru.vorxa.ncraftmedia.dto
 
-data class Post (
+data class Event (
     val id: Long,
     val author: String,
     val content: String,
     val created: Long = System.currentTimeMillis()/1000,
+    var address: String = "",
+    var location: Pair<Double, Double>,
     var likedByMe: Boolean = false,
     var commentedByMe: Boolean = false,
     var sharedByMe: Boolean = false,
