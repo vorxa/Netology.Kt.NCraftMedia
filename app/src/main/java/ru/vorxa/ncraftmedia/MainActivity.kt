23e42b664cb90cd026a0b7e97db2f8b1341e0856
vorxa <vorxa@yandex.ru>
2019-11-27 15:24:07 +0700

    20191127-1524

--- app/src/main/java/ru/vorxa/ncraftmedia/MainActivity.kt
+++ app/src/main/java/ru/vorxa/ncraftmedia/MainActivity.kt
@@ -1,95 +1,46 @@
 package ru.vorxa.ncraftmedia
 
-import android.content.Intent
-import android.graphics.Color
-import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.ImageButton
-import android.widget.TextView
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
-import ru.vorxa.ncraftmedia.dto.Event
-import ru.vorxa.ncraftmedia.dto.Post
-import ru.vorxa.ncraftmedia.dto.PostType
 import ru.vorxa.ncraftmedia.utils.*
 import androidx.recyclerview.widget.LinearLayoutManager
+import io.ktor.client.request.get
+import kotlinx.coroutines.*
 import ru.vorxa.ncraftmedia.adapter.PostAdapter
-import java.net.URI
+import ru.vorxa.ncraftmedia.client.Api
+import ru.vorxa.ncraftmedia.dto.Post
+import kotlin.coroutines.CoroutineContext
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CoroutineScope by MainScope() {
+    private val job = SupervisorJob()
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + job
 
-    // homework_5
+    // homework_6
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val postsList = postsList()
+        fetchData()
 
-        with(container) {
-            layoutManager = LinearLayoutManager(this@MainActivity)
-            adapter = PostAdapter(postsList)
+//        with(container) {
+//            layoutManager = LinearLayoutManager(this@MainActivity)
+//            adapter = PostAdapter(list = posts)
+//        }
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cancel()
+    }
+
+    fun fetchData() = launch {
+        val posts = withContext(Dispatchers.IO) {
+            Api.client.get<List<Post>>(Api.url)
         }
-
+        Toast.makeText(this@MainActivity, "Length: ${posts.size}", Toast.LENGTH_LONG).show()
     }
 }
-/*
-        val post = post1.copy(author = "Кот Матроскин", likedByMe = true)
-
-        dateTextView.text = humanizeTime(currentTime - post.created)
-        authorTextView.text = post.author
-        contentTextView.text = post.content
-
-        setButtons(post.likedByMe, post.likes, likesTextView, likeButton, "favorite")
-        setButtons(post.sharedByMe, post.shares, sharesTextView, shareButton, "share")
-        setButtons(post.commentedByMe, post.comments, commentsTextView, commentButton, "comment")
-
-        likeButton.setOnClickListener{
-            post.likedByMe=!post.likedByMe
-            if (post.likedByMe) {
-                post.likes += 1
-            } else {
-                post.likes -= 1
-            }
-            setButtons(post.likedByMe, post.likes, likesTextView, likeButton, "favorite")
-        }
-        commentButton.setOnClickListener{
-            post.commentedByMe=!post.commentedByMe
-            if (post.commentedByMe) {
-                post.comments += 1
-            } else {
-                post.comments -= 1
-            }
-            setButtons(post.commentedByMe, post.comments, commentsTextView, commentButton, "comment")
-        }
-        shareButton.setOnClickListener{
-            post.sharedByMe=!post.sharedByMe
-            if (post.sharedByMe) {
-                post.shares += 1
-            } else {
-                post.shares -= 1
-            }
-            setButtons(post.sharedByMe, post.shares, sharesTextView, shareButton, "share")
-        }
-        geoButton.setOnClickListener {
-            val location = Uri.parse("geo:${post.location?.first},${post.location?.second}")
-            val intent = Intent(Intent.ACTION_VIEW).apply { data = location }
-            startActivity(intent)
-
-        }
-
-
-    }
-
-    private fun setButtons(byMe: Boolean, count: Int, textView: TextView, buttonView: ImageButton, type: String) {
-        textView.text = if (count > 0) count.toString() else ""
-        val image = when(type) {
-            "favorite" -> if (byMe) R.drawable.ic_favorite_red_24dp else R.drawable.ic_favorite_gray_24dp
-            "comment" -> if (byMe) R.drawable.ic_comment_red_24dp else R.drawable.ic_comment_gray_24dp
-            else -> if (byMe) R.drawable.ic_share_red_24dp else R.drawable.ic_share_gray_24dp
-        }
-        buttonView.setImageResource(image)
-        textView.setTextColor(if (byMe) Color.RED else Color.GRAY)
-    }
-}
-*/
---
+++ app/src/main/java/ru/vorxa/ncraftmedia/client/Api.kt
@@ -0,0 +1,28 @@
+package ru.vorxa.ncraftmedia.client
+
+import io.ktor.client.HttpClient
+import io.ktor.client.features.json.GsonSerializer
+import io.ktor.client.features.json.JsonFeature
+import io.ktor.client.features.logging.LogLevel
+import io.ktor.client.features.logging.Logger
+import io.ktor.client.features.logging.Logging
+import io.ktor.client.features.logging.SIMPLE
+import io.ktor.http.ContentType
+
+object Api {
+    const val url = "https://raw.githubusercontent.com/vorxa/Netology.Kt.Gson/master/output.json"
+
+    val client = HttpClient {
+        install(JsonFeature) {
+            acceptContentTypes = listOf(
+                ContentType.Text.Plain,
+                ContentType.Application.Json
+            )
+            serializer = GsonSerializer()
+        }
+        install(Logging){
+            logger = Logger.SIMPLE
+            level = LogLevel.INFO
+        }
+    }
+}
